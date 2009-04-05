2e316328b9efd1ac935caae8cf687acef15a9b84
baseonmars <dan@baseonmars.co.uk>
2009-04-05 22:40:20 +0100

    added version for twitter gem

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,7 @@ Rails::Initializer.run do |config|
   config.gem "authlogic", :version => '>2.0.4'
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com", :version => '>2.9.1'
 	config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
-	config.gem "twitter"
+	config.gem "twitter", :version => '0.4.2'
 	config.gem 'iridesco-time-warp', :lib => 'time_warp', :source => "http://gems.github.com"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
