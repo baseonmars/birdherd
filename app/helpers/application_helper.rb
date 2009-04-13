d5ed03ae31306c8cd268ad62b13058f489fb6b7c
baseonmars <dan@baseonmars.co.uk>
2009-04-13 22:27:52 +0100

    can see who replied to a status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def birdherd_users_string(status)
+    users = status.replies.inject([]) { |acc,r| acc << r.birdherd_user }.compact.inject([]){ |acc,u| acc << u.login }.join(', ')
+    users.blank? ? "" : "replied to by: " + users
+  end
 end
