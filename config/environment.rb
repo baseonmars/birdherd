df47890f9c09501e849b69283bc062d3feebc1a3
baseonmars <dan@baseonmars.co.uk>
2009-04-14 21:47:37 +0100

    Better logging output... TODO put into a lib

--- config/environment.rb
+++ config/environment.rb
@@ -44,3 +44,29 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+module ActiveSupport
+  class BufferedLogger
+    def add(severity, message = nil, progname = nil, &block)
+      return if @level > severity
+      message = (message || (block && block.call) || progname).to_s
+
+      level = {
+        0 => "DEBUG",
+        1 => "INFO",
+        2 => "WARN",
+        3 => "ERROR",
+        4 => "FATAL"
+      }[severity] || "U"
+
+      message = "[%s: %s] %s" % [level,
+        Time.now.strftime("%m%d %H:%M:%S"),
+        message]
+
+      message = "#{message}\n" unless message[-1] == ?\n
+      buffer << message
+      auto_flush
+      message
+    end
+  end
+end
