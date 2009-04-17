5ca07986fc0a2b60dd6300eee74e8a52853a17bb
baseonmars <dan@baseonmars.co.uk>
2009-04-18 00:14:38 +0100

    Signup page now has sign in and sign up forms

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,6 +3,7 @@ class UserSessionsController < ApplicationController
   before_filter :require_user, :only => :destroy
   def new
     @user_session = UserSession.new
+    @user = User.new
   end
 
   def create
