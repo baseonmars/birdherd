9abdf229266163585f1025f714a3180f8c1e72e6
baseonmars <dan@baseonmars.co.uk>
2009-04-18 02:03:47 +0100

    twitter user profile pic and screen name helpers, stub friendly time helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     users = status.replies.inject([]) { |acc,r| acc << r.birdherd_user }.compact.inject([]){ |acc,u| acc << u.login }.join(', ')
     users.blank? ? "" : "replied to by: " + users
   end
+  
+  def friendly_time(time)
+    time.strftime("%H:%M %a %B %Y")
+  end
+    
 end
--- app/helpers/twitter_users_helper.rb
+++ app/helpers/twitter_users_helper.rb
@@ -1,2 +1,24 @@
 module TwitterUsersHelper
+    
+  def twitter_user_link(twitter_user)
+    link_to "@#{twitter_user.screen_name}",
+     "http://twitter.com/#{twitter_user.screen_name}", 
+     :alt => profile_text(twitter_user),
+     :title => profile_text(twitter_user)
+  end
+   
+  def twitter_user_profile_image(twitter_user)
+    unless twitter_user.profile_image_url.blank?
+      link_to image_tag(twitter_user.profile_image_url), 
+        "http://twitter.com/#{twitter_user.screen_name}", 
+        :alt => profile_text(twitter_user),
+        :title => profile_text(twitter_user),
+        :class => 'profile-image' 
+    end
+  end
+  
+  private
+    def profile_text(twitter_user)
+      "@#{twitter_user.screen_name}'s profile"
+    end
 end
