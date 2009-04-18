9a98a6bf035123da836276a8c29ae79d8669008d
baseonmars <dan@baseonmars.co.uk>
2009-04-18 02:11:48 +0100

    email address added to poster by and reply to username list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def birdherd_users_string(status)
-    users = status.replies.inject([]) { |acc,r| acc << r.birdherd_user }.compact.inject([]){ |acc,u| acc << u.login }.join(', ')
-    users.blank? ? "" : "replied to by: " + users
+  def reply_users_string(status)
+    users = status.replies.inject([]) { |acc,r| acc << r.birdherd_user }.compact
+    reply_string = users.inject([]){ |acc,u| acc << "#{u.login} (#{u.email})" }.join(', ')
+    users.blank? ? "" : reply_string
   end
   
   def friendly_time(time)
