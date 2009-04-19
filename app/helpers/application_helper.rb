df3d991f543c9636160cdbf4295d8237477b1b82
opsb <oliver@opsb.co.uk>
2009-04-19 14:39:21 +0100

    Cleaned up formatting in friendly_time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
   def reply_users_string(status)
     users = status.replies.inject([]) { |acc,r| acc << r.birdherd_user }.compact
     reply_string = users.inject([]){ |acc,u| acc << "#{u.login} (#{u.email})" }.join(', ')
@@ -10,21 +11,15 @@ module ApplicationHelper
     
     seconds_ago = Time.now - time
     minutes_ago = (seconds_ago / 60).to_i
-
-    if time > 1.minutes.ago
-      return "a minute ago"
-    elsif time > 1.hours.ago
-      return "#{minutes_ago} minutes ago"
-    elsif time > Time.now.at_beginning_of_day
-      return time.strftime("%I:%M%p")
-    elsif time > 1.days.ago.at_beginning_of_day
-      return time.strftime("yesterday at %I:%M%p")
-    elsif time > 1.weeks.ago.at_beginning_of_day
-      return time.strftime("%A at %I:%M%p")
-    elsif time > Time.now.at_beginning_of_month
-      return time.strftime("%e %B")
-    else
-      return time.strftime("%e %B, %Y")
+    
+    case
+      when  time > 1.minutes.ago;                     "a minute ago"
+      when  time > 1.hours.ago;                       "#{minutes_ago} minutes ago"
+      when  time > Time.now.at_beginning_of_day;      time.strftime("%I:%M%p")
+      when  time > 1.days.ago.at_beginning_of_day;    time.strftime("yesterday at %I:%M%p")
+      when  time > 1.weeks.ago.at_beginning_of_day;   time.strftime("%A at %I:%M%p")
+      when  time > Time.now.at_beginning_of_month;    time.strftime("%e %B")
+      else                                            time.strftime("%e %B, %Y")
     end
     
   end
