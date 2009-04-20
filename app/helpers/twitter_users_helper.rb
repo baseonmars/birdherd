bea66d936fa33df4bd9b011f5d9522630ffee83d
baseonmars <dan@baseonmars.co.uk>
2009-04-20 22:03:20 +0100

    cleaning up the appearance of statuses and dm listings

--- app/helpers/twitter_users_helper.rb
+++ app/helpers/twitter_users_helper.rb
@@ -1,19 +1,19 @@
 module TwitterUsersHelper
     
   def twitter_user_link(twitter_user)
-    link_to "@#{twitter_user.screen_name}",
+    link_to "#{twitter_user.screen_name}",
      "http://twitter.com/#{twitter_user.screen_name}", 
      :alt => profile_text(twitter_user),
      :title => profile_text(twitter_user)
   end
    
-  def twitter_user_profile_image(twitter_user)
+  def twitter_user_profile_image(twitter_user, options={})
     unless twitter_user.profile_image_url.blank?
       link_to image_tag(twitter_user.profile_image_url), 
         "http://twitter.com/#{twitter_user.screen_name}", 
-        :alt => profile_text(twitter_user),
+        {:alt => profile_text(twitter_user),
         :title => profile_text(twitter_user),
-        :class => 'profile-image' 
+        :class => 'profile-image' }.merge(options)
     end
   end
   
