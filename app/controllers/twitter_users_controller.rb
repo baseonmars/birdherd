f22a9a97d1f8a9f31a2e227dd4abff2d0fe27a17
baseonmars <dan@baseonmars.co.uk>
2009-05-05 23:52:34 +0100

    changed direct_messages method to user new with_limit

--- app/controllers/twitter_users_controller.rb
+++ app/controllers/twitter_users_controller.rb
@@ -26,7 +26,7 @@ class TwitterUsersController < ApplicationController
     if @account && @account.owned_by?(@current_user)    
       @timeline = @account.friends_timeline_with_limit(30, :include => [:replies, :poster] )
       @replies = @account.replies(:include => [:replies, :poster], :limit => 30)
-      @direct_messages = @account.direct_messages(:include => [:sender, :recipient])[0...30]
+      @direct_messages = @account.direct_messages_with_limit(30, :include => [:sender, :recipient])
       @status = @account.statuses.new
     end
   end  
