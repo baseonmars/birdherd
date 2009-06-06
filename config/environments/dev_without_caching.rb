27f3cee8a1acb077283ab65d08daa9bc2c57b776
baseonmars <dan@baseonmars.co.uk>
2009-06-06 23:57:33 +0100

    turned caching on by default in development, made a dev_without_caching environment

---
+++ config/environments/dev_without_caching.rb
@@ -0,0 +1,4 @@
+require 'development'
+
+config.cache_classes = false
+config.action_controller.perform_caching = false
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@
 # In the development environment your application's code is reloaded on
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
-config.cache_classes = false
+config.cache_classes = true
 
 # Log error messages when you accidentally call methods on nil.
 config.whiny_nils = true
@@ -11,7 +11,7 @@ config.whiny_nils = true
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
-config.action_controller.perform_caching             = false
+config.action_controller.perform_caching             = true
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = true
@@ -23,8 +23,6 @@ SITE[:url] = 'http://devbirdherd.com:3000'
 SITE[:api_key] = 'FhNZIOFLllHvpB2VlEXAA'
 SITE[:api_secret] = 'MHXEDctGtssS7VQjLwHHA5lgPlAnWy0Xi6NO2nnRc'
                                         
-config.cache_classes = true    
-config.action_controller.perform_caching = true
-config.cache_store = :file_store, '/tmp/bh_cache'    
+  
                                            
 config.gem "ruby-debug"                                                                                                   
