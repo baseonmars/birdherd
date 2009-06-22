88973c7c55125d46585165cc2bacf131785df051
baseonmars <dan@baseonmars.co.uk>
2009-06-22 21:54:31 +0100

    setting release tag to v0.1.1a

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,4 @@ set :domain, "thebirdherd.com"
 set :deploy_to, "/var/www/vhosts/thebirdherd.com"
 set :repository, 'git@github.com:baseonmars/birdherd.git'
 set :web_command, 'sudo /etc/init.d/apache2'
+set :revision, 'v0.1.1a'
