e9ad43e35383c600502fb84d2977bc88127f242b
opsb <oliver@opsb.co.uk>
2009-06-28 17:04:44 +0100

    Ziggy is now disabled for tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,4 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-Ziggy::active = true
+Ziggy::active = false
