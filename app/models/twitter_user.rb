1e86c2fba1a60d825fc4c2df1bd98996aaba130d
baseonmars <dan@baseonmars.co.uk>
2009-07-08 00:05:28 +0100

    added Ziggy caching to the 4 status related methods on TwitterUser

--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -2,7 +2,7 @@ class TwitterUser < ActiveRecord::Base
   extend Ziggy
   has_and_belongs_to_many :users
   has_many :statuses, :class_name => "TwitterStatus", :foreign_key => "poster_id"
-  cached :friends_timeline  
+  cached :friends_timeline, :direct_messages_sent, :direct_messages_recieved, :mentions  
   
   def friends_timeline
     TwitterStatus.merge_all account_api.friends_timeline(:count => 30)
