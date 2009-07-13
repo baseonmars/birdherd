ac7bdb4d24a0032bb8613d49b3b8c5183c63a2cb
baseonmars <dan@baseonmars.co.uk>
2009-07-13 23:22:45 +0100

    Refactored javascript for greater readability

--- doc/html/javascript/application.js
+++ doc/html/javascript/application.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
-	
+	showHideControls();
+});
+
+var showHideControls = function () {
 	$('#toggle-status-update').replaceWith('<div id="toggle-status-update" ><span>Toggle status updates</span></div>')
-	   
+
 	$('#toggle-status-update').click(function (){
 		$('#status-update').slideToggle('fast',function(){
 			if ($(this).css('display') === 'none') {
@@ -9,12 +12,12 @@ $(document).ready(function () {
 					'background-image': 'url(images/show-post-update.gif)',
 					'margin': '-4px 0 0 0'
 				});
-				
 			} else {
 				$('#toggle-status-update').css('margin-top', '0').find('span').css({
 					'background-image': 'url(images/hide-post-update.gif)',
-					'margin': '0'});
+					'margin': '0'
+				});
 			}
 		});
 	});
-});
+};
