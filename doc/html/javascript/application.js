9b785d032399df65188061013f529c530bbd99d6
baseonmars <dan@baseonmars.co.uk>
2009-07-16 23:52:30 +0100

    Action links now fill the correct text into the response textarea Made the actions tab with pressed down button look nicer in safari

--- doc/html/javascript/application.js
+++ doc/html/javascript/application.js
@@ -1,48 +1,95 @@
 $(document).ready(function () {
-	showHideControls();
-	setup_response('direct_message', function(el, text) {
-		 return 'd ' + getScreenName(el).replace(/@/,'') + ' ' + text  
-	 });
-	setup_response('re_tweet',function(el, text) {
-		 return text + ' (via ' + getScreenName(el) + ')';
-	 }); 
-	setup_response('reply',function(el,text) {
-		return getScreenName(el)+'';
-	});
-});                    
+	Birdherd.UI.init(); 
+});       
 
-var getScreenName = function(el) {
-	return $(el).parents('.tweet').find('.screen_name').attr('title')
-}
+var Birdherd = {};
 
-
-var showHideControls = function () {
-	$('#toggle-status-update').replaceWith('<div id="toggle-status-update" ><span>Toggle status updates</span></div>')
-
-	$('#toggle-status-update').click(function (){
-		$('#status-update').toggle('fast',function(){
-			if ($(this).css('display') === 'none') {
-				$('#toggle-status-update').css('margin-top', '-2px').find('span').css({
-					'background-image': 'url(images/show-post-update.gif)',
-					'position': 'relative',
-					'top': '-5px'
-				});
-			} else {
-				$('#toggle-status-update').css('margin-top', '0').find('span').css({
-					'background-image': 'url(images/hide-post-update.gif)',
-					'top': '-1px'
-				});
-			}
+Birdherd.UI = (function(){         
+	// Init
+	var init;
+	
+	init = function() {
+		this.showHideControls();
+		
+		this.setResponseAction('direct_message', function(el) {
+			return 'd '+getScreenName(el).replace(/@/,'')+' ';
 		});
-	});
-};
+		this.setResponseAction('re_tweet',function(el, text) {
+			return text+' (via '+getScreenName(el)+')';
+		}); 
+		this.setResponseAction('reply',function(el) {
+			return getScreenName(el)+' ';
+		});
+	};
+	
+	//Private
+	var getScreenName, setupResponse, setCaretToEnd;
 
-var setup_response = function (type, formatter) {
-	$('.'+type).click( function (){
-		var parents = $(this).parents('.tweet');
-		parents.find('.status_text').text(formatter(this, parents.find('.text').text())); 
-		parents.find('.actions li').removeClass('down');
-		$(this).addClass('down');
-		return false;                            
-	});                                                                   
-}
+	getScreenName = function(el) {
+		return $(el).parents('.tweet').find('.sender').attr('title')
+	};
+
+	// modified from http://parentnode.org/javascript/working-with-the-cursor-position/
+	setCaretToEnd = function(obj) { 
+		var pos = $(obj).text().length
+		if(obj.createTextRange) { 
+			/* Create a TextRange, set the internal pointer to
+			a specified position and show the cursor at this
+			position
+			*/ 
+			var range = obj.createTextRange(); 
+			range.move("character", pos); 
+			range.select(); 
+		} else if(obj.selectionStart) { 
+			/* Gecko is a little bit shorter on that. Simply
+			focus the element and set the selection to a
+			specified position
+			*/ 
+			obj.focus();         
+			obj.setSelectionRange(pos, pos); 
+		} 
+	};
+	
+	return {
+		showHideControls: function () {
+			$('#toggle-status-update').replaceWith('<div id="toggle-status-update" ><span>Toggle status updates</span></div>')
+
+			$('#toggle-status-update').click(function (){
+				$('#status-update').toggle('fast',function(){
+					if ($(this).css('display') === 'none') {
+						$('#toggle-status-update').css('margin-top', '-2px').find('span').css({
+							'background-image': 'url(images/show-post-update.gif)',
+							'position': 'relative',
+							'top': '-5px'
+						});
+					} else {
+						$('#toggle-status-update').css('margin-top', '0').find('span').css({
+							'background-image': 'url(images/hide-post-update.gif)',
+							'top': '-1px'
+						});
+					}
+				});
+			});
+		},
+		setResponseAction: function (type, formatter) {
+			$('.'+type).click( function (){
+				var parents = $(this).parents('.tweet');
+				parents.find('.status_text').text(formatter(this, parents.find('.text').focus().text())); 
+				parents.find('.actions li').removeClass('down');
+				setCaretToEnd(parents.find('.status_text').get(0));
+				$(this).addClass('down');
+				return false;                            
+			});                                                                   
+		},
+		init: init
+	};
+}());
+
+
+
+
+
+
+
+
+
