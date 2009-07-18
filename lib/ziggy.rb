0c722d2fd8caff5fda4096ca20862b2870e21426
opsb <oliver@opsb.co.uk>
2009-07-18 14:54:25 +0100

    Replaced ziggy with ziggy2

--- lib/ziggy.rb
+++ lib/ziggy.rb
@@ -1,44 +1,61 @@
-module Ziggy
+module Ziggy2
 
-  @@active = true
-
-  def self.active=(isActive)
-    @@active = isActive
-  end
-
-  def cached(*cachable_methods, &block)
-    return unless @@active
-    @cached_methods ||= []
-    @cached_methods_enhanced ||= []
-    @cached_methods += cachable_methods
-
-    @key_generator = block
-    class << self
-      def method_added(method)
-        return unless @cached_methods.include? method        
-        return if @cached_methods_enhanced.include? method
-        @cached_methods_enhanced << method
-        method_without_cache = "#{method}_without_cache".to_sym
-        class_eval do 
-          alias_method method_without_cache, method 
-          define_method(method) do |*args|
-            invocation_key = "#{method}#{ args.collect{ |a| a.to_s } }"
-            keygen = self.class.key_generator
-            differentiator = (keygen.call(self) unless keygen.nil?) || ""
-            key = differentiator + invocation_key
-            return Rails.cache.read(key) if Rails.cache.exist?(key)
-            result = send(method_without_cache, *args)
-            Rails.cache.write(key, result, :expires_in => 2.5.minutes)
-            result
-          end
-        end
-        logger.debug "Caching added to #{self}.#{method}"
-      end  
+  def self.included(base)
+    base.extend ClassMethods
+    base.instance_eval do
+      @should_be_cached = []
+      @cached = []  
+      @keygens = {}
+      @expire_after = {}
     end
   end
 
-  def key_generator
-    @key_generator
+  module ClassMethods
+    def cached(*cachable_methods, &block)
+      opts = (cachable_methods.pop if cachable_methods.last.kind_of? Hash) || {}
+      @should_be_cached += cachable_methods
+      cachable_methods.each do |m| 
+        @keygens[m] = block
+        @expire_after[m] = opts[:expire_after] || 2.5.minutes
+      end
+    end
+    
+    def should_be_cached?(method)
+      @should_be_cached.include? method
+    end
+    
+    def cached?(method)
+      @cached.include? method
+    end
+    
+    def method_added(method)
+      return unless should_be_cached?(method) && !cached?(method)
+      @cached << method
+      method_without_cache = "#{method}_without_cache".to_sym
+      class_eval do 
+        alias_method method_without_cache, method 
+        define_method(method) do |*args|
+          key = self.class.build_key(self, method, args)
+          return Rails.cache.read(key) if Rails.cache.exist?(key)
+          result = send(method_without_cache, *args)
+          Rails.cache.write(key, result, :expires_in => expire_after(method))
+          result
+        end
+      end
+      p "caching added to #{method}"
+      logger.debug "Caching added to #{self}.#{method}"
+    end    
+    
+    def expire_after(method)
+      @expire_after[method]
+    end
+    
+    def build_key(instance, method, args)
+      invocation_key = "#{method}#{ args.collect{ |a| a.to_s } }"
+      keygen = @keygens[method]
+      differentiator = (keygen.call(instance) unless keygen.nil?) || ""
+      differentiator + invocation_key
+    end    
   end
-
+  
 end
