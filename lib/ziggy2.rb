9880a3e3629bdd6cb0a51610612ace946f5ed34e
opsb <oliver@opsb.co.uk>
2009-07-18 13:00:10 +0100

    ziggy2 has improved checks to determine if a method should have caching added

--- lib/ziggy2.rb
+++ lib/ziggy2.rb
@@ -16,22 +16,33 @@ module Ziggy2
       @key_generator = block
     end
     
+    def should_be_cached?(method)
+      @cached_methods.include? method
+    end
+    
+    def cached?(method)
+      @cached_methods_enhanced.include? method
+    end
+    
     def method_added(method)
-      return unless @cached_methods.include? method        
-      return if @cached_methods_enhanced.include? method
+      return unless should_be_cached?(method) && !cached?(method)
       @cached_methods_enhanced << method
       method_without_cache = "#{method}_without_cache".to_sym
       class_eval do 
         alias_method method_without_cache, method 
         define_method(method) do |*args|
-          invocation_key = "#{method}#{ args.collect{ |a| a.to_s } }"
-          keygen = self.class.key_generator
-          differentiator = (keygen.call(self) unless keygen.nil?) || ""
-          key = differentiator + invocation_key
+          key = build_key(method, args)
           return Rails.cache.read(key) if Rails.cache.exist?(key)
           result = send(method_without_cache, *args)
           Rails.cache.write(key, result, :expires_in => 2.5.minutes)
           result
+        end
+      
+        def build_key(method, args)
+          invocation_key = "#{method}#{ args.collect{ |a| a.to_s } }"
+          keygen = self.class.key_generator
+          differentiator = (keygen.call(self) unless keygen.nil?) || ""
+          differentiator + invocation_key
         end
       end
       logger.debug "Caching added to #{self}.#{method}"
