b2fee06214834618b944d732de4191fc303a5f3a
baseonmars <dan@baseonmars.co.uk>
2009-07-31 13:42:42 +0100

    changed the way the character count javascript is applied (now uses jQuery's live method)
    bumped deploy tag to v0.2.6a

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,4 +3,4 @@ set :domain, "thebirdherd.com"
 set :deploy_to, "/var/www/vhosts/thebirdherd.com"
 set :repository, 'git@github.com:baseonmars/birdherd.git'
 set :web_command, 'sudo /etc/init.d/apache2'
-set :revision, 'v0.2.6a'
+set :revision, 'v0.2.7a'
